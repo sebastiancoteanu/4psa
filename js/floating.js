71b719daa34a7e3b182337f5b266bf7fec866a1e
DESKTOP-D64SFO8\Coteanu-Sebastian <sebastiancoteanu@gmail.com>
2017-10-25 19:58:15 +0300

    Use resize functionality and create functionalities for creating and removing placeholder items to maintain the grid.

--- js/floating.js
+++ js/floating.js
@@ -5,7 +5,7 @@
 	var container = document.getElementById('container');
 
 	var minMargin = 10;
-	var numberOfBoxes = 9;
+	var numberOfBoxes = 10;
 
 	initialPopulate();
 
@@ -17,10 +17,57 @@
 		}
 	}
 
+	function getNumberOfChildrenToCreate(numberOfItems, index) {
+		return Math.ceil(numberOfItems / index) * index - numberOfItems;
+	}
+
+	function appendChildren(numberOfItems) {
+		var i;
+		for(i = 0; i < numberOfItems; i++) {
+			var childToCreate = document.createElement('div');
+			childToCreate.className = 'after';
+			container.appendChild(childToCreate);
+		}
+	}
+
+	createPlaceholders();
+
+	// Creating 'blank' divs to fill last row if need.
+	function createPlaceholders() {
+		var children = document.getElementsByClassName("imageHolder"),
+			numberOfChildren = children.length,
+			numberOfChildrenToCreate = 0,
+			i;
+
+		//	Value is true if children inside parent container fit on single line.
+		var isOnSingleLine = true;
+
+		for(i = 1; i < numberOfChildren; i++) {
+			if(children[i].offsetTop !== children[i - 1].offsetTop) {
+				numberOfChildrenToCreate = getNumberOfChildrenToCreate(numberOfChildren, i);
+				isOnSingleLine = false;
+				break;
+			}
+		}
+
+		//  Change parent 'display' for minimum margin between children items.
+		if(isOnSingleLine) {
+			container.style.display = 'block';
+			return;
+		}
+
+		container.style.display = 'flex';
+		appendChildren(numberOfChildrenToCreate);
+	}
+
 	$("#container").resizable();
 
 	$("#container").on( "resize", function() {
-		
+		var createdChildren = document.getElementsByClassName("after");
+		while(createdChildren[0]) {
+			container.removeChild(createdChildren[0]);
+		}
+		createPlaceholders();
 	});
 
 }())
